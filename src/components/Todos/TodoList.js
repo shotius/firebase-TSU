de3e7336570a1f40d9814aef893d1af5801927a6
shotius <sh.archemashvili@gmail.com>
2021-06-21 11:46:10 +0400

    changed todo done btn

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -13,7 +13,7 @@ export default function TodoList({
           <span className={`fb-40 ${todo.completed ? 'todo-done': ''}`} >{todo.title} </span>
           <span ><b>{todo.completed ? 'finished': 'active'}</b> </span>
           <span>
-          <button onClick={() => updateTodohandler(todo)} className="m10">Update</button>
+          <button onClick={() => updateTodohandler(todo)} className="m10">{todo.completed ? 'make active' : 'Done'}</button>
           <button onClick={() => deleteTodoHandler(todo.id)} className="m10">Delete</button>
           </span>
         </li>
